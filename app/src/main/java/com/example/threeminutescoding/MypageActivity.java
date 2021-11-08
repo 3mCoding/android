1bbc13d60dcecf1f98d2c20655307602c7fa1ee7
Hajin Kim <rhrnak624@naver.com>
2021-11-08 22:44:27 +0900

    마이페이지 정보 연결

--- app/src/main/java/com/example/threeminutescoding/MypageActivity.java
+++ app/src/main/java/com/example/threeminutescoding/MypageActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.threeminutescoding.user.UserInfo;
+
 public class MypageActivity extends AppCompatActivity {
     TextView txtTitle;
+    TextView txtName, txtEmail, txtRank, txtProblem, txtDate;
+    String name, email, date;
+    int problem, rank;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,25 @@ public class MypageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mypage);
         txtTitle = findViewById(R.id.txtTitle);
         txtTitle.setText("마이페이지");
+
+        txtName = findViewById(R.id.txtName);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtRank = findViewById(R.id.txtRank);
+        txtProblem = findViewById(R.id.txtProblem);
+        txtDate = findViewById(R.id.txtDate);
+
+        name = UserInfo.getName();
+        date = UserInfo.getJoinData();
+
+        problem = UserInfo.getStep();
+        rank = UserInfo.getRank();
+
+        txtName.setText(name);
+        txtDate.setText("가입한 날짜 : " + date);
+        txtRank.setText("순위 : " + rank + "위");
+        txtProblem.setText("푼 문제 수 : " +problem);
+
+
+
     }
 }

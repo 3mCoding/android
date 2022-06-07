13782e16a938e7d8bc01df8f556c1b8ca954d69e
Hajin Kim <rhrnak624@naver.com>
2022-06-07 18:35:54 +0900

    예상 도착 시간 입력 대화창 생성

---
+++ app/src/main/java/com/sirius/threeminutescoding/QuestionRecommendationDialog.java
@@ -0,0 +1,56 @@
+package com.sirius.threeminutescoding;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class QuestionRecommendationDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_question_recommendation, null);
+
+        builder.setView(view);
+
+        final Dialog dialog = builder.create();
+
+        Spinner spinner = view.findViewById(R.id.spinner_level);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.level, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        final EditText editText = view.findViewById(R.id.edit_expect_minutes);
+
+        view.findViewById(R.id.btn_confirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int expectMinutes = Integer.parseInt(editText.getText().toString());
+                Log.d("예상 소요 시간", expectMinutes+"");
+                dialog.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.btn_skip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        return dialog;
+
+    }
+}
